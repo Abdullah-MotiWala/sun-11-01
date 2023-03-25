fae6fec0e19e8e80132b6edd30832d57edc517f4
abdullah-motiwala <motiwalaabdullah100@gmail.com>
2023-03-25 00:59:38 -0700

    data types, if else, arthimetic operations, logical operators

---
+++ index.js
@@ -0,0 +1,92 @@
+// camelCase
+// snake_case
+
+
+// Data Types
+
+// String "hello world"
+// Number 250
+// Array [1,2,3] | ["a",1, [1],{},ture]
+// Object {name:"abdullah",
+//  fname:"junaid",
+//  surname:"motiwala"}
+// Boolean true | false
+// var | let | const
+
+// Var could reassigned as well as re declared
+// var my_name = "abdullah"
+// var my_name = "M.Abdullah"
+
+// // Let could't be redeclared although could re-assign
+// let f_name = "junaid"
+// f_name = "M.Junaid"
+
+// // Const could neither be re-assigned not re-declared
+// const sur_name = "motiwala"
+// // sur_name = "MotiWala", it is not possible
+// console.log(my_name, f_name, sur_name)
+
+
+// let my_name = 'hammad'
+// const pi = 3.142
+// my_name = 'Shaffan'
+
+// Arthimetic Operations
+// + -* / 
+// let class_1 = "09-11"
+// let class_2 = "11-01"
+// let combined_classes = class_1 + " " + class_2
+// console.log("concetenated", combined_classes)
+
+
+// let total = 1;
+// total = 1 + 1
+
+// total = 5 - 2
+// total = 2 * 4
+// total = 5 / 2
+// total = 5 % 2
+// // console.log(total)
+// let number_value = 250
+// let string_value = "250"
+
+// console.log(number_value === string_value, typeof number_value, typeof string_value)
+// let concetenated_values = number_value + 1 + string_value
+// console.log(concetenated_values)
+
+
+// let my_string = "bano-qabil"
+// let start_timing = 09
+// let end_timing = 11
+// let is_ramadan = true
+// let students = ["Awais", "Yousuf"]
+// let sessions = { session_1: "09-11", session2: '11-01' }
+
+// let schedule = my_string + " " + start_timing + "-" + end_timing
+// console.log(schedule)
+
+
+// Conditions
+
+// let is_ramadan = false
+// if (is_ramadan === true) {
+//     console.log("Happy Ramadan")
+// }
+
+let before_matric = false
+let after_matric = true
+let graduated = false
+
+if (before_matric) {
+    console.log("Maze hi maze")
+} else if (after_matric) {
+    console.log("thode se maze")
+} else if (graduated) {
+    console.log("Maze khtam hne wale h")
+} else {
+    console.log("No maze found")
+}
+
+
+let your_age = prompt("Enter Your Age")
+alert("You can visit website")
